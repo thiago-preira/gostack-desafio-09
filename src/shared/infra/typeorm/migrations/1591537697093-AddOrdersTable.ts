8e2f4bb7964b5cfbf03d0e845cee00152979179e
Thiago Pereira <thiago@pop-os.localdomain>
2020-06-07 15:59:31 +0100

    DEsafio 09

---
+++ src/shared/infra/typeorm/migrations/1591537697093-AddOrdersTable.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class AddOrdersTable1591537697093 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'customer_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'CustomerId',
+            columnNames: ['customer_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'customers',
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders');
+  }
+}
